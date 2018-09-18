19ffe7f60662dfe92b70b392c2c173debb53c7ee
Karen Eva Jónsdóttir <karenevajonsdottir@Karens-MacBook-Pro.local>
2018-09-18 09:51:18 +0000

    algrim

---
+++ tiletraveler.py
@@ -0,0 +1,7 @@
+#First we create the file map from 1,1 to 3,3
+#Then make possible moves for each file
+#Request the user for an input, which should be a destination
+#Respond to that input. (If its not valid we re-request an input)
+#If player reaches 3,3 we print "victory!" and then exit the program.
+
+
